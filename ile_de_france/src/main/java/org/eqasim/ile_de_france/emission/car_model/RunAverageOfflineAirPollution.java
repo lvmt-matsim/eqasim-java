9fc0fa808fc747a87d0168db13d0fcfe0773322f
biaoyin <yihu580124@gmail.com>
2025-03-14 15:04:39 +0100

    Add hbefa_network generation

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/emission/car_model/RunAverageOfflineAirPollution.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/emission/car_model/RunAverageOfflineAirPollution.java
@@ -23,19 +23,17 @@ import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehicleUtils;
 
 public class RunAverageOfflineAirPollution {
-
-	final static String scenarioID = "ile_de_france_1pm";
-	final static String inputFilePath = "./ile_de_france/src/main/java/org/eqasim/ile_de_france/emission/input";
-	final static String outputPath = "./simulation_output/" + scenarioID;
-//	static final String configFile = outputPath + "./output_config.xml";
-	static final String eventsFile = outputPath + "./output_events.xml.gz";
-/*	static final String hbefaFileCold =  inputFilePath+ "./EFA_ColdStart_Subsegm_2021ParcAuto_LCA_filter.csv";
-	static final String hbefaFileWarm =  inputFilePath+ "./EFA_HOT_Subsegm_2021ParcAuto_LCA_filter.csv";*/
-	static final String hbefaFileCold =  inputFilePath+ "./2022_IDF_EFA_ColdStart_Vehcat_Average_OnlyCar_Marjolaine.csv";
-	static final String hbefaFileWarm =  inputFilePath+ "./2022_IDF_EFA_HOT_Vehcat_Average_OnlyCar_Marjolaine.csv";
+	final static String inputHbefaPath = "./ile_de_france/src/main/java/org/eqasim/ile_de_france/emission/input";
+	final static String inputFilePath = "G:/lvmt_mlannes/simulation_Biao/emission_average_IDF_100pct/60iter";
 
 
-	static final String emissionEventOutputFileName = outputPath + "./emissions_average_default_vehicle_marjolaine.xml.gz";
+	static final String eventsFile = inputFilePath + "./output_events.xml.gz";
+
+	static final String hbefaFileCold =  inputHbefaPath+ "./2022_IDF_EFA_ColdStart_Vehcat_Average_OnlyCar_Marjolaine.csv";
+	static final String hbefaFileWarm =  inputHbefaPath+ "./2022_IDF_EFA_HOT_Vehcat_Average_OnlyCar_Marjolaine.csv";
+
+
+	static final String emissionEventOutputFileName = inputFilePath + "./emissions_average_default_vehicle.xml.gz";
 
 
 	static public void main(String[] args) {		// Create config group for emissions
@@ -50,10 +48,10 @@ public class RunAverageOfflineAirPollution {
 
         // or create a new and simple config file
 		Config config = ConfigUtils.createConfig();
-		config.vehicles().setVehiclesFile(inputFilePath + "./output_vehicles.xml"); // see new method for output_vehicle.xml in matsim simulation
-		config.network().setInputFile(outputPath+ "./hbefa_network.xml");
+		config.vehicles().setVehiclesFile(inputFilePath + "./output_vehicles.xml.gz"); // see new method for output_vehicle.xml in matsim simulation
+		config.network().setInputFile(inputFilePath+ "./hbefa_network.xml");
 //		config.network().setInputFile(outputPath+ "./output_network.xml.gz");
-		config.plans().setInputFile(outputPath + "./output_plans.xml.gz");
+		config.plans().setInputFile(inputFilePath + "./output_plans.xml.gz");
 //		config.global().setCoordinateSystem("EPSG:2154");
 		config.parallelEventHandling().setNumberOfThreads(null);
 		config.parallelEventHandling().setEstimatedNumberOfEvents(null);
@@ -64,41 +62,11 @@ public class RunAverageOfflineAirPollution {
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		EventsManager eventsManager = EventsUtils.createEventsManager();
 
-		//network mapping: way 1- using default function of complete network info.  Attention: some road limit speed like "URB/Access/15, URB/Access/35, URB/Access/45" are replaced by "URB/Access/30, URB/Access/40, URB/Access/50", as no such setting in Hbefa factor files, BYIN 03-2023
+		//network mapping: way 1-default function
 //		OsmHbefaMapping abc = OsmHbefaMapping.build();
 //		Network network = scenario.getNetwork();
 //		abc.addHbefaMappings(network);
 //      new NetworkWriter(network).write(scenarioDirectory+ "./hbefa_network.xml.gz");
-
-		// network settings: way 2: self-definition for simplicity
-/*		for (Link link : scenario.getNetwork().getLinks().values()) {
-			double freespeed;
-			freespeed = link.getFreespeed();
-			if(freespeed <= 8.34){ //30kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/Access/30");
-			} else if(freespeed <= 13.89){ //50kmh
-				double lanes = link.getNumberOfLanes();
-				if(lanes <= 1.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Local/50");
-				} else if(lanes <= 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Distr/50");
-				} else if(lanes > 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Trunk-City/50");
-				} else{
-					throw new RuntimeException("NoOfLanes not properly defined");
-				}
-			} else if(freespeed <= 19.45){ //70kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-City/70");
-			} else if(freespeed <= 25){ //90kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-Nat./90");
-			} else if(freespeed <= 30.6){ //110kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "RUR/MW/110");
-			} else if(freespeed > 30.6){ //faster
-				link.getAttributes().putAttribute("hbefa_road_type", "RUR/MW/130");
-			} else{
-				throw new RuntimeException("Link not considered...");
-			}
-		}*/
 
         // vehicle settings if without vehicle type information from the input vehicle type file, and the settings is cooresponding to network modes settings
 		// check the output_vehicles.xml to set below the same the vehicle type
@@ -158,6 +126,6 @@ public class RunAverageOfflineAirPollution {
 
 		emissionEventWriter.closeFile();
 
-		new MatsimVehicleWriter( scenario.getVehicles() ).writeFile( outputPath+ "./vehicles_types.xml.gz" );
+		//new MatsimVehicleWriter( scenario.getVehicles() ).writeFile( inputFilePath+ "./vehicles_types.xml.gz" );
 	}
 }
